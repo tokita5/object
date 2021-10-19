9267079603c9b337ea2f7e60a5ed3e68898f4236
tokita5 <soutensikku55@gmail.com>
2021-10-19 13:48:52 +0900

    2021/10/19変更した内容

--- src/basic/inspection/ViewResult.java
+++ src/basic/inspection/ViewResult.java
@@ -1,21 +1,19 @@
 package basic.inspection;
 
-import student.Student;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ViewResult {
     public static void main(String[] args) {
         List<Result> resultList = new ArrayList<>();
-        resultList.add(new Result("202110190311", "土岐田裕也",
-                "2021-10-19T11:11:11", 36.5, 85, 80, 90));
+        resultList.add(new Result(
+                "202110190311",
+                "土岐田裕也",
+                "2021-10-19T11:11:11",
+                36.5, 85, 80, 90));
 
         for (Result result : resultList) {
             System.out.println(result);
         }
     }
 }
-
-
-
